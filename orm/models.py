a85b9281490de0fff2a1d107588d8d694dffbb4e
florimondmanca <florimond.manca@gmail.com>
2019-03-25 21:07:31 +0100

    add .count()

--- orm/models.py
+++ orm/models.py
@@ -110,13 +110,13 @@ class QuerySet:
 
                 model_cls = self.model_cls
                 if related_parts:
-                    # Add any implied select_related
+                    #  Add any implied select_related
                     related_str = "__".join(related_parts)
                     if related_str not in select_related:
                         select_related.append(related_str)
 
                     # Walk the relationships to the actual model class
-                    # against which the comparison is being made.
+                    #  against which the comparison is being made.
                     for part in related_parts:
                         model_cls = model_cls.fields[part].to
 
@@ -156,13 +156,22 @@ class QuerySet:
             select_related=related,
         )
 
+    async def count(self) -> int:
+        expr = self.build_select_expression()
+        expr = sqlalchemy.func.count().select().select_from(expr)
+        count, = await self.database.fetch_one(expr)
+        return count
+
     async def all(self, **kwargs):
         if kwargs:
             return await self.filter(**kwargs).all()
 
         expr = self.build_select_expression()
         rows = await self.database.fetch_all(expr)
-        return [self.model_cls.from_row(row, select_related=self._select_related) for row in rows]
+        return [
+            self.model_cls.from_row(row, select_related=self._select_related)
+            for row in rows
+        ]
 
     async def get(self, **kwargs):
         if kwargs:
@@ -261,8 +270,8 @@ class Model(typesystem.Schema, metaclass=ModelMetaclass):
 
         # Instantiate any child instances first.
         for related in select_related:
-            if '__' in related:
-                first_part, remainder = related.split('__', 1)
+            if "__" in related:
+                first_part, remainder = related.split("__", 1)
                 model_cls = cls.fields[first_part].to
                 item[first_part] = model_cls.from_row(row, select_related=[remainder])
             else:
