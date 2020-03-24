a92ee4f7399de988b9b46fffe8311e783585a883
Tom Christie <tom@tomchristie.com>
2020-03-24 15:21:56 +0000

    ModelField has a validator instance, rather than subclassing

--- orm/fields.py
+++ orm/fields.py
@@ -14,10 +14,10 @@ class ModelField:
     ) -> None:
         if primary_key:
             kwargs["allow_null"] = True
-        super().__init__(**kwargs)  # type: ignore
         self.primary_key = primary_key
         self.index = index
         self.unique = unique
+        self.validator = self.get_validator(**kwargs)
 
     def get_column(self, name: str) -> sqlalchemy.Column:
         column_type = self.get_column_type()
@@ -33,6 +33,9 @@ class ModelField:
             unique=self.unique,
         )
 
+    def get_validator(self, **kwargs) -> typesystem.Field:
+        raise NotImplementedError()  # pragma: no cover
+
     def get_column_type(self) -> sqlalchemy.types.TypeEngine:
         raise NotImplementedError()  # pragma: no cover
 
@@ -43,62 +46,92 @@ class ModelField:
         return value
 
 
-class String(ModelField, typesystem.String):
+class String(ModelField):
     def __init__(self, **kwargs):
         assert "max_length" in kwargs, "max_length is required"
         super().__init__(**kwargs)
 
+    def get_validator(self, **kwargs) -> typesystem.Field:
+        return typesystem.String(**kwargs)
+
     def get_column_type(self):
-        return sqlalchemy.String(length=self.max_length)
+        return sqlalchemy.String(length=self.validator.max_length)
 
 
-class Text(ModelField, typesystem.Text):
+class Text(ModelField):
+    def get_validator(self, **kwargs) -> typesystem.Field:
+        return typesystem.Text(**kwargs)
+
     def get_column_type(self):
         return sqlalchemy.Text()
 
 
-class Integer(ModelField, typesystem.Integer):
+class Integer(ModelField):
+    def get_validator(self, **kwargs) -> typesystem.Field:
+        return typesystem.Integer(**kwargs)
+
     def get_column_type(self):
         return sqlalchemy.Integer()
 
 
-class Float(ModelField, typesystem.Float):
+class Float(ModelField):
+    def get_validator(self, **kwargs) -> typesystem.Field:
+        return typesystem.Float(**kwargs)
+
     def get_column_type(self):
         return sqlalchemy.Float()
 
 
-class Boolean(ModelField, typesystem.Boolean):
+class Boolean(ModelField):
+    def get_validator(self, **kwargs) -> typesystem.Field:
+        return typesystem.Boolean(**kwargs)
+
     def get_column_type(self):
         return sqlalchemy.Boolean()
 
 
-class DateTime(ModelField, typesystem.DateTime):
+class DateTime(ModelField):
+    def get_validator(self, **kwargs) -> typesystem.Field:
+        return typesystem.DateTime(**kwargs)
+
     def get_column_type(self):
         return sqlalchemy.DateTime()
 
 
-class Date(ModelField, typesystem.Date):
+class Date(ModelField):
+    def get_validator(self, **kwargs) -> typesystem.Field:
+        return typesystem.Date(**kwargs)
+
     def get_column_type(self):
         return sqlalchemy.Date()
 
 
-class Time(ModelField, typesystem.Time):
+class Time(ModelField):
+    def get_validator(self, **kwargs) -> typesystem.Field:
+        return typesystem.Time(**kwargs)
+
     def get_column_type(self):
         return sqlalchemy.Time()
 
 
-class JSON(ModelField, typesystem.Any):
+class JSON(ModelField):
+    def get_validator(self, **kwargs) -> typesystem.Field:
+        return typesystem.Any()
+
     def get_column_type(self):
         return sqlalchemy.JSON()
 
 
-class ForeignKey(ModelField, typesystem.Field):
+class ForeignKey(ModelField):
     def __init__(self, to, allow_null: bool = False):
         super().__init__(allow_null=allow_null)
         self.to = to
 
-    def validate(self, value, strict=False):
+    def validate(self, value):
         return value.pk
+
+    def get_validator(self, **kwargs) -> typesystem.Field:
+        return typesystem.Any()
 
     def get_constraints(self):
         fk_string = self.to.__tablename__ + "." + self.to.__pkname__
