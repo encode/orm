f007170689cdd49f988a05544f275ae3a6eadab2
Amin Alaee <mohammadamin.alaee@gmail.com>
2022-07-23 18:57:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,8 @@ setup(
         "Operating System :: OS Independent",
         "Topic :: Internet :: WWW/HTTP",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
